a879ee7c4c5e8b03488882c81db6bd573fa8968d
mroberts-panw <104632309+mroberts-panw@users.noreply.github.com>
2022-08-24 14:46:08 -0400

    Update s3.tf

--- terraform/aws/s3.tf
+++ terraform/aws/s3.tf
@@ -134,7 +134,6 @@ resource "aws_s3_bucket" "logs" {
     git_commit           = "d68d2897add9bc2203a5ed0632a5cdd8ff8cefb0"
     git_file             = "terraform/aws/s3.tf"
     git_last_modified_at = "2020-06-16 14:46:24"
-    git_last_modified_by = "nimrodkor@gmail.com"
     git_org              = "bridgecrewio"
     git_repo             = "terragoat"
     yor_trace            = "01946fe9-aae2-4c99-a975-e9b0d3a4696c"
